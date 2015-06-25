b73b182f30ae6a8a23bd9c4a9dc38eb5414b97e8
Karl von Randow <karl@cactuslab.com>
2015-06-26 11:20:46 +1200

    VCL: varnish-local takes an argument to specify which root VCL to use

--- bin/varnish-local.sh
+++ bin/varnish-local.sh
@@ -5,10 +5,13 @@ VARNISHD=/usr/local/opt/varnish/sbin/varnishd
 INSTANCE_NAME=/usr/local/var/varnish
 USER=$(id -un)
 
-ROOT_VCL=charles.vcl
+ROOT_VCL=$1
+if [ "x$ROOT_VCL" == "x" ]; then
+	ROOT_VCL=charles
+fi
 
 VCL_DIR=$(pwd)/$(dirname $0)/../etc/varnish
-CONFIG=$VCL_DIR/$ROOT_VCL
+CONFIG=$VCL_DIR/${ROOT_VCL}.vcl
 
 VARNISH_ADMIN_PORT=2000
 VARNISH_PORT=8081
@@ -17,4 +20,4 @@ sudo $VARNISHD -F -n "$INSTANCE_NAME" -f "$CONFIG" -s malloc,1G -T 127.0.0.1:$VA
 	-p feature=+esi_disable_xml_check \
 	-p feature=+esi_ignore_other_elements \
 	-p workspace_client=256K \
-	-p vcl_dir="$VCL_DIR/"
+	-p vcl_dir="$VCL_DIR"
