e610e570327fc8f27388ba87a78b369bd765fb03
Karl von Randow <karl@cactuslab.com>
2015-06-26 11:19:54 +1200

    VCL: script to start a local Varnish server

---
+++ bin/varnish-local.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Start a local Varnish server with the Letterboxd configuration
+
+VARNISHD=/usr/local/opt/varnish/sbin/varnishd
+INSTANCE_NAME=/usr/local/var/varnish
+USER=$(id -un)
+
+ROOT_VCL=charles.vcl
+
+VCL_DIR=$(pwd)/$(dirname $0)/../etc/varnish
+CONFIG=$VCL_DIR/$ROOT_VCL
+
+VARNISH_ADMIN_PORT=2000
+VARNISH_PORT=8081
+
+sudo $VARNISHD -F -n "$INSTANCE_NAME" -f "$CONFIG" -s malloc,1G -T 127.0.0.1:$VARNISH_ADMIN_PORT -a 0.0.0.0:$VARNISH_PORT -u "$USER" \
+	-p feature=+esi_disable_xml_check \
+	-p feature=+esi_ignore_other_elements \
+	-p workspace_client=256K \
+	-p vcl_dir="$VCL_DIR/"
