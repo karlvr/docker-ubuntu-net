ee75621d39758c159dafb45d7ed40fd09c6abdae
Karl von Randow <karl@xk72.com>
2016-08-31 06:51:01 +1200

    Remove legacy pgpool2 setup

--- init/init-db-server.sh
+++ init/init-db-server.sh
@@ -8,12 +8,3 @@ fi
 if [ ! -d /home/letterboxd ]; then
 	useradd -m -d /home/letterboxd -s /bin/bash letterboxd
 fi
-
-# pgpool2 setup
-
-apt-get install postgresql-9.3-pgpool2
-
-su - postgres -c psql template1 <<EOF
-CREATE EXTENSION pgpool_regclass; 
-CREATE EXTENSION pgpool_recovery;
-EOF
