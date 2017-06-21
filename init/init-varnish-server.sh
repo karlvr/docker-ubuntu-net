d732e363c9d951810c82dea79c132586de347194
Karl von Randow <karl@xk72.com>
2017-06-21 12:41:34 +1200

    nginx setup

---
+++ init/init-varnish-server.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -eu
+# Varnish server
+
+# NB: this script is incomplete
+
+# nginx
+
+apt-get install nginx
+ln -s /opt/letterboxd/etc/nginx/varnish.conf /etc/nginx/conf.d/
+ln -s /opt/letterboxd/etc/nginx/sites-available/letterboxd /etc/nginx/sites-enabled/
