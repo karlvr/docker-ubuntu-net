13f66c6a7768a39bdb579bed2c3ca99f110d412d
Karl von Randow <karl@xk72.com>
2017-07-17 14:38:59 +1200

    Tools: add -s switch to use staging jars

--- libexec/tool-base.sh
+++ libexec/tool-base.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 DEV=0
+STAGING=0
 CONTINUE=0
 
-while getopts ":dc" opt; do
+while getopts ":dsc" opt; do
   case $opt in
     d)
       DEV=1
+      ;;
+    s)
+      STAGING=1
       ;;
     c)
 	  CONTINUE=1
@@ -21,7 +25,23 @@ shift $((OPTIND-1))
 
 TMPDIR=
 
-if [ $DEV == 0 ]; then
+if [ $STAGING == 1 ]; then
+	echo "Using staging tools jar"
+	TOOLSJAR=/srv/tools/staging/letterboxd-tools-1.0-SNAPSHOT-jar-with-dependencies.jar
+	if [ "x$HOME" != "x" -a "x$TMPDIRNAME" != "x" ]; then
+		TMPDIR="$HOME"/work/"$TMPDIRNAME"
+	elif [ "x$TMPDIRNAME" != "x" ]; then
+		TMPDIR=/tmp/$TMPDIRNAME/tmp
+	fi
+	TOMCATLIBDIR=/opt/tomcat/lib
+elif [ $DEV == 1 ]; then
+	echo "Using dev tools jar"
+	TOOLSJAR=~/Development/let0596_letterboxd/tools/tools/target/letterboxd-tools-1.0-SNAPSHOT-jar-with-dependencies.jar
+	if [ "x$TMPDIRNAME" != "x" ]; then
+		TMPDIR=~/tmp/$TMPDIRNAME
+	fi
+	TOMCATLIBDIR=~/Library/Tomcat/Current/lib
+else
 	TOOLSJAR=/srv/tools/letterboxd-tools-1.0-SNAPSHOT-jar-with-dependencies.jar
 	if [ "x$HOME" != "x" -a "x$TMPDIRNAME" != "x" ]; then
 		TMPDIR="$HOME"/work/"$TMPDIRNAME"
@@ -29,12 +49,6 @@ if [ $DEV == 0 ]; then
 		TMPDIR=/tmp/$TMPDIRNAME/tmp
 	fi
 	TOMCATLIBDIR=/opt/tomcat/lib
-else
-	TOOLSJAR=~/Development/let0596_letterboxd/tools/tools/target/letterboxd-tools-1.0-SNAPSHOT-jar-with-dependencies.jar
-	if [ "x$TMPDIRNAME" != "x" ]; then
-		TMPDIR=~/tmp/$TMPDIRNAME
-	fi
-	TOMCATLIBDIR=~/Library/Tomcat/Current/lib
 fi
 
 if [ "x$TMPDIR" != "x" ]; then
